5c8442ad20b512449d88af015959a7789bda156e
Tim Dysinger <tim@dysinger.net>
2012-06-20 22:02:34 -1000

    Updated joyent

--- Box/Provider/Joyent.hs
+++ Box/Provider/Joyent.hs
@@ -1,6 +1,7 @@
 module Box.Provider.Joyent where
 
-main :: IO ()
-main = do
-  -- TODO use cmd args to parse options only for joyent
-  return ()
+import Box.Types
+import System.Console.CmdArgs
+
+mode = Joyent
+       &= help "Joyent SmartCloud"
