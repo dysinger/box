bbbfac63262b108ed1e59368162bba7f82ce29de
Tim Dysinger <tim@dysinger.net>
2012-06-22 09:55:24 -1000

    Switch to shelly and more cmdargs explicit work

--- Box/SmartBox.hs
+++ Box/SmartBox.hs
@@ -1,33 +1,53 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE RecordWildCards #-}
 
 module Box.SmartBox where
 
-import           Box.Types
 import           Box.Provider.VirtualBox
-
+import           Box.Types
 import           Crypto.Conduit
-import qualified Data.ByteString         as DB
-import qualified Data.ByteString.Base16  as DBB
-import qualified Data.ByteString.Char8   as DBC
-import qualified Data.ByteString.Lazy    as DBL
+import qualified Data.ByteString                 as DB
+import qualified Data.ByteString.Base16          as DBB
+import qualified Data.ByteString.Char8           as DBC
+import qualified Data.ByteString.Lazy            as DBL
 import           Data.Conduit
-import           Data.Conduit.Binary     hiding (dropWhile, lines)
+import           Data.Conduit.Binary             hiding (dropWhile, lines)
 import           Data.Digest.Pure.MD5
-import qualified Data.Serialize          as DS
-import           HSH
-import           Network.HTTP.Conduit    hiding (def, path)
+import qualified Data.Serialize                  as DS
+import qualified Data.Text.Lazy                  as DLT
+import           Network.HTTP.Conduit            hiding (def, path)
+import qualified System.Console.CmdArgs.Explicit as SCCE
+import           System.Console.CmdArgs.Explicit hiding (mode)
 import           System.Directory
 
+-- TODO add some subcommands (create, update, delete) with their own flags
+
+-- TODO get a nice looking help working
+
+mode :: Mode [(String,String)]
+mode = modes "smartos" [] "SmartOS Management" [ createMode
+                                               , deleteMode ]
+
+createMode :: Mode [(String,String)]
+createMode = SCCE.mode "create" [] "create a smartos instance"
+             (flagArg (upd "file") "FILE") []
+  where upd msg x v = Right $ (msg,x):v
+
+deleteMode :: Mode [(String,String)]
+deleteMode = SCCE.mode "delete" [] "delete a smartos instance"
+             (flagArg (upd "file") "FILE") []
+  where upd msg x v = Right $ (msg,x):v
+
 dispatch :: Command -> IO ()
-dispatch vbox@VBox{..} = do
-  putStrLn $ show sync
-  p <- properties
-  i <- release
-  let s = SmartOS { smartos_name  = "smartos"
-                  , smartos_iso   = i
-                  , smartos_props = p }
-  downloadISO s
-  createOrUpdate s
+-- dispatch vbox@VBox{..} = do
+--   putStrLn $ show sync
+--   p <- properties
+--   i <- release
+--   let s = SmartOS { smartos_name  = "smartos"
+--                   , smartos_iso   = i
+--                   , smartos_props = p }
+--   downloadISO s
+--   createOrUpdate s
 dispatch _ = return ()
 
 release :: IO ISO
@@ -43,7 +63,7 @@ release = do
 
 downloadISO :: SmartOS -> IO ()
 downloadISO s = do
-  createDirectoryIfMissing True $ path s
+  createDirectoryIfMissing True $ home s
   exists <- doesFileExist $ isoPath s
   if exists then checksum else download
   where
@@ -58,16 +78,16 @@ downloadISO s = do
         bsrc $$ sinkFile $ isoPath s
       downloadISO s
 
-createOrUpdate :: SmartOS -> IO ()
-createOrUpdate s = do
-  exists <- run $ "VBoxManage showvminfo smartos" :: IO Bool
-  if exists /= True then create s else update s
-  where create s = do createVm s
-                      createIde
-                      createDisk s
-                      update s
-        update s = do attachISO s
-                      attachDisk s
-                      updateCpuMem
-                      updateBootOrder
-                      updateNetwork s
+-- createOrUpdate :: SmartOS -> IO ()
+-- createOrUpdate s = do
+--   exists <- vBoxManageVmCmd "smartos" [ "showinfo" ]
+--   if exists /= True then create s else update s
+--   where create s = do createVm s
+--                       createIde
+--                       createDisk s
+--                       update s
+--         update s = do attachISO s
+--                       attachDisk s
+--                       updateCpuMem
+--                       updateBootOrder
+--                       updateNetwork s
