4dab8bcf50588f7f92aff35af0b064b2debd325e
Tim Dysinger <tim@dysinger.net>
2012-06-22 09:55:35 -1000

    Trying out cmdargs explicit modes

--- Box/Provider/Joyent.hs
+++ Box/Provider/Joyent.hs
@@ -1,7 +1,15 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
 module Box.Provider.Joyent where
 
-import Box.Types
-import System.Console.CmdArgs
+import           Box.Types
+import           System.Console.CmdArgs.Explicit hiding (mode)
+import qualified System.Console.CmdArgs.Explicit as SCCE
 
-mode = Joyent
-       &= help "Joyent SmartCloud"
+mode :: Mode [(String,String)]
+mode = SCCE.mode "joyent" [] "Joyent Management" (flagArg (upd "file") "FILE")
+      [ flagOpt "world" ["hello","h"]    (upd "world")    "WHO" "World argument"
+      , flagReq         ["greeting","g"] (upd "greeting") "MSG" "Greeting to give"
+      , flagHelpSimple (("help",""):)
+      ]
+    where upd msg x v = Right $ (msg,x):v
--- Main.hs
+++ Main.hs
@@ -1,35 +1,10 @@
-{-# LANGUAGE RecordWildCards #-}
-
-import qualified Box.SmartBox            as SmartBox
-import           Box.Types
-import           System.Console.CmdArgs
-import           System.Environment
+import qualified Box.SmartBox                    as SmartBox
+import qualified Box.Provider.Joyent             as Joyent
+import           System.Console.CmdArgs.Explicit
 
 main :: IO ()
 main = do
-  args <- getArgs
-  opts <- (if null args then withArgs ["--help"] else id) $ cmdArgs modes'
-  dispatch opts
+  processArgs ms >>= putStrLn . show
   where
-    modes'      = modes
-                  [ VBox { sync = def &= help "Syncronize Releases" }
-                  , Joyent
-                  ]
-                  &= program name'
-                  &= help about'
-                  &= summary (info' ++ ", " ++ copyright')
-                  &= helpArg       [ explicit, name "help",    name "h" ]
-                  &= verbosityArgs [ explicit, name "Verbose", name "V" ] []
-                  &= versionArg    [ explicit
-                                   , name "version"
-                                   , name "v"
-                                   , summary info' ]
-    name'       = "box"
-    version'    = "0.1.0.0"
-    info'       = name' ++ " version " ++ version'
-    about'      = "A command line application to manage boxes."
-    copyright'  = "Copyright © 2012 Positive Inertia, LLC."
-
-dispatch :: Command -> IO ()
-dispatch v@VBox{..} = SmartBox.dispatch v
-dispatch _ = return ()
+    ms = modes "smartos" [] "SmartOS Management" [ SmartBox.mode
+                                                 , Joyent.mode ]
